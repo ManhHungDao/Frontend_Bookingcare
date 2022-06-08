e53a7124d90e1b8cba4cae6fc56a21fcd1689dd8
hung dao <hungpepi2001@gmail.com>
2022-06-08 23:45:47 +0700

    add icon optinal

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -25,13 +25,13 @@ class HomeHeader extends Component {
                   <b className="header-title">Cơ sở y tế</b>
                 </div>
                 <span className="subs-title">Chọn bệnh viện phòng khám</span>
-              </div>{" "}
+              </div>
               <div className="child-content">
                 <div>
                   <b className="header-title">Bác sĩ</b>
                 </div>
                 <span className="subs-title">Chọn bác sĩ giỏi</span>
-              </div>{" "}
+              </div>
               <div className="child-content">
                 <div>
                   <b className="header-title">Gói khám</b>
@@ -42,6 +42,75 @@ class HomeHeader extends Component {
             <div className="right-container">
               <i className="fas fa-question-circle"></i>Hỗ trợ
               <div className="flag">VN</div>
+            </div>
+          </div>
+        </div>
+        <div className="home-header-banner">
+          <div className="content-up">
+            <div className="title title1">NỀN TẢNG Y TẾ</div>
+            <div className="title title2">CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+            <div className="search">
+              <i className="fas fa-search"></i>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="content-down">
+            <div className="options">
+              <div className="options-child">
+                <div className="wrap-child">
+                  <div className="icon-child child1"></div>
+                </div>
+                <div className="text-child">
+                  Khám <br /> Chuyên khoa
+                </div>
+              </div>
+              <div className="options-child">
+                <div className="icon-child child2"></div>
+                <div className="text-child">
+                  Khám
+                  <br /> từ xa
+                </div>
+              </div>
+              <div className="options-child">
+                <div className="icon-child child3"></div>
+                <div className="text-child">
+                  Khám
+                  <br /> tổng quát
+                </div>
+              </div>
+              <div className="options-child">
+                <div className="icon-child child4"></div>
+                <div className="text-child">
+                  Xét nghiệm <br /> y học
+                </div>
+              </div>
+              <div className="options-child">
+                <div className="icon-child child5"></div>
+                <div className="text-child">
+                  Sức khỏe
+                  <br /> tinh thần
+                </div>
+              </div>
+              <div className="options-child">
+                <div className="icon-child child6"></div>
+                <div className="text-child">
+                  Khám
+                  <br /> nha Khoa
+                </div>
+              </div>
+              <div className="options-child">
+                <div className="icon-child child7"></div>
+                <div className="text-child">
+                  Gói <br /> phẫu thuật
+                </div>
+              </div>
+              <div className="options-child">
+                <div className="icon-child child8"></div>
+                <div className="text-child">
+                  Sản phẩm
+                  <br /> y tế
+                </div>
+              </div>
             </div>
           </div>
         </div>
