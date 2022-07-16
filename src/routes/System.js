84238826cafed180e70b73f709d006ba329fe118
hung dao <hungpepi2001@gmail.com>
2022-07-16 23:09:54 +0700

    delete  user redux

--- src/routes/System.js
+++ src/routes/System.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect, Route, Switch } from "react-router-dom";
-import UserManage from "../containers/System/UserManage";
 import UserRedux from "../containers/System/Admin/UserRedux";
 import Header from "../containers/Header/Header";
 import ManageDoctor from "../containers/System/Admin/ManageDoctor";
@@ -16,8 +15,7 @@ class System extends Component {
         <div className="system-container">
           <div className="system-list">
             <Switch>
-              <Route path="/system/user-manage" component={UserManage} />
-              <Route path="/system/user-redux" component={UserRedux} />
+              <Route path="/system/user-manage" component={UserRedux} />
               <Route path="/system/manage-doctor" component={ManageDoctor} />
               <Route path="/system/manage-clinic" component={ManageClinic} />
               <Route
