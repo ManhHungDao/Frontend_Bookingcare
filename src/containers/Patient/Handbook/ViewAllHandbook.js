9fbba5030fdb23b2e49e6a71c7e6bf2d4d5747bb
hung dao <hungpepi2001@gmail.com>
2022-11-13 17:26:38 +0700

    add handle viewmore handbook

--- src/containers/Patient/Handbook/ViewAllHandbook.js
+++ src/containers/Patient/Handbook/ViewAllHandbook.js
@@ -5,27 +5,55 @@ import { languages } from "../../../utils";
 import { FormattedMessage } from "react-intl";
 import { toast } from "react-toastify";
 import { withRouter } from "react-router-dom";
-import { useEffect } from "react";
-import { getListHandbook } from "../../../services/userService";
-import { useState } from "react";
-import SubHeader from "../../HomePage/SubHeader";
+import { useEffect, useState } from "react";
+import { getHandBookHome } from "../../../services/userService";
+import HomeHeader from "../../HomePage/HomeHeader";
+import "./ViewAllHandbook.scss";
 
 const ViewAllHandbook = () => {
   const [listHandbook, setListHandbook] = useState([]);
   const [loadMore, setLoadMore] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
-      const res = await getListHandbook();
+      const res = await getHandBookHome();
       if (res && res.errCode === 0) {
-        setListHandbook = res.data;
+        setListHandbook(res.data);
       }
     };
     fetchData().catch((err) => console.log(err));
-  }, [loadMore]);
+  }, []);
 
   return (
     <>
-      <SubHeader isShowSupport={true} name="Cẩm nang" />
+      {/* <HomeHeader /> */}
+      <div className="hanbook-container">
+        <div className="container">
+          <h1 className="pt-5">Cẩm Nang</h1>
+          <div className="content-body-handbook">
+            <div className="row">
+              {listHandbook &&
+                listHandbook.map((item) => (
+                  <div className="col-12 body-content-item" key={item.id}>
+                    <div className="handbook-img">
+                      <img
+                        src={item?.image ? item.image : ""}
+                        alt={item?.title ? item.title : ""}
+                      />
+                    </div>
+                      <span className="handbook-name">
+                        {item?.title ? item.title : ""}
+                      </span>
+                  </div>
+                ))}
+              <div className="col-12" style={{padding:0}}>
+                <span className="view-more">
+                  Xem thêm<i className="fas fa-caret-down"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
